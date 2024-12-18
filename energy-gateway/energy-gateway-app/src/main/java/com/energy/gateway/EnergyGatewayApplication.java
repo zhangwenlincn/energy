64c8329ae7c25c750ff3e0f36d8fd35edaae03e6
zhangwenlin <zhangwenlin@skiffenergy.com>
2024-12-18 16:11:17 +0800

    commit message

---
+++ energy-gateway/energy-gateway-app/src/main/java/com/energy/gateway/EnergyGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.energy.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author zhangwenlin
+ * @since 2024-12-18
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EnergyGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EnergyGatewayApplication.class, args);
+    }
+}
