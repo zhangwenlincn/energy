ec984151e84e7a445c6d72b3dd876b1e1eb1194d
zhangwenlin <zhangwenlin@skiffenergy.com>
2024-12-20 10:51:10 +0800

    commit message

--- energy-spring-boot-starter/src/main/java/com/energy/starter/processor/NacosConfigInitializerProcessor.java
+++ energy-spring-boot-starter/src/main/java/com/energy/starter/processor/NacosConfigInitializerProcessor.java
@@ -1,7 +1,9 @@
 package com.energy.starter.processor;
 
+import org.apache.commons.logging.Log;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.env.EnvironmentPostProcessor;
+import org.springframework.boot.logging.DeferredLogFactory;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.core.env.ConfigurableEnvironment;
@@ -10,11 +12,16 @@ import org.springframework.core.io.ClassPathResource;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author zhangwenlin
@@ -35,7 +42,6 @@ public class NacosConfigInitializerProcessor implements EnvironmentPostProcessor
     private final static String SPRING_CLOUD_NACOS_CONFIG_USERNAME = "spring.cloud.nacos.config.username";
     private final static String SPRING_CLOUD_NACOS_CONFIG_PASSWORD = "spring.cloud.nacos.config.password";
     private final static String SPRING_PROFILES_ACTIVE = "spring.profiles.active";
-    private final static String POINT = ".";
 
     @Override
     public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
@@ -45,31 +51,28 @@ public class NacosConfigInitializerProcessor implements EnvironmentPostProcessor
         activeProperties.put(SPRING_PROFILES_ACTIVE, active);
         environment.getPropertySources().addLast(new MapPropertySource(SPRING_PROFILES_ACTIVE, activeProperties));
         Map<String, Object> nacosProperties = getResource(active, classPathResource);
-        nacosProperties.entrySet().removeIf(x -> Objects.isNull(x.getValue()));
         environment.getPropertySources().addLast(new MapPropertySource(NACOS_PROPERTIES, nacosProperties));
     }
 
     public Map<String, Object> getResource(String property, ClassPathResource classPathResource) {
-        try {
+        try (InputStream inputStream = classPathResource.getInputStream()) {
             Properties prop = new Properties();
             HashMap<String, Object> map = new HashMap<>();
-            InputStream inputStream = classPathResource.getInputStream();
             prop.load(inputStream);
-            map.put(SPRING_CLOUD_NACOS_DISCOVERY_SERVER_ADDR, prop.getProperty(property + POINT + SPRING_CLOUD_NACOS_DISCOVERY_SERVER_ADDR));
-            map.put(SPRING_CLOUD_NACOS_DISCOVERY_NAMESPACE, prop.getProperty(property + POINT + SPRING_CLOUD_NACOS_DISCOVERY_NAMESPACE));
-            map.put(SPRING_CLOUD_NACOS_DISCOVERY_GROUP, prop.getProperty(property + POINT + SPRING_CLOUD_NACOS_DISCOVERY_GROUP));
-            map.put(SPRING_CLOUD_NACOS_DISCOVERY_USERNAME, prop.getProperty(property + POINT + SPRING_CLOUD_NACOS_DISCOVERY_USERNAME));
-            map.put(SPRING_CLOUD_NACOS_DISCOVERY_PASSWORD, prop.getProperty(property + POINT + SPRING_CLOUD_NACOS_DISCOVERY_PASSWORD));
-            map.put(SPRING_CLOUD_NACOS_CONFIG_SERVER_ADDR, prop.getProperty(property + POINT + SPRING_CLOUD_NACOS_CONFIG_SERVER_ADDR));
-            map.put(SPRING_CLOUD_NACOS_CONFIG_NAMESPACE, prop.getProperty(property + POINT + SPRING_CLOUD_NACOS_CONFIG_NAMESPACE));
-            map.put(SPRING_CLOUD_NACOS_CONFIG_GROUP, prop.getProperty(property + POINT + SPRING_CLOUD_NACOS_CONFIG_GROUP));
-            map.put(SPRING_CLOUD_NACOS_CONFIG_USERNAME, prop.getProperty(property + POINT + SPRING_CLOUD_NACOS_CONFIG_USERNAME));
-            map.put(SPRING_CLOUD_NACOS_CONFIG_PASSWORD, prop.getProperty(property + POINT + SPRING_CLOUD_NACOS_CONFIG_PASSWORD));
+            map.put(SPRING_CLOUD_NACOS_DISCOVERY_SERVER_ADDR, prop.getProperty(property + "." + SPRING_CLOUD_NACOS_DISCOVERY_SERVER_ADDR));
+            map.put(SPRING_CLOUD_NACOS_DISCOVERY_NAMESPACE, prop.getProperty(property + "." + SPRING_CLOUD_NACOS_DISCOVERY_NAMESPACE));
+            map.put(SPRING_CLOUD_NACOS_DISCOVERY_GROUP, prop.getProperty(property + "." + SPRING_CLOUD_NACOS_DISCOVERY_GROUP));
+            map.put(SPRING_CLOUD_NACOS_DISCOVERY_USERNAME, prop.getProperty(property + "." + SPRING_CLOUD_NACOS_DISCOVERY_USERNAME));
+            map.put(SPRING_CLOUD_NACOS_DISCOVERY_PASSWORD, prop.getProperty(property + "." + SPRING_CLOUD_NACOS_DISCOVERY_PASSWORD));
+            map.put(SPRING_CLOUD_NACOS_CONFIG_SERVER_ADDR, prop.getProperty(property + "." + SPRING_CLOUD_NACOS_CONFIG_SERVER_ADDR));
+            map.put(SPRING_CLOUD_NACOS_CONFIG_NAMESPACE, prop.getProperty(property + "." + SPRING_CLOUD_NACOS_CONFIG_NAMESPACE));
+            map.put(SPRING_CLOUD_NACOS_CONFIG_GROUP, prop.getProperty(property + "." + SPRING_CLOUD_NACOS_CONFIG_GROUP));
+            map.put(SPRING_CLOUD_NACOS_CONFIG_USERNAME, prop.getProperty(property + "." + SPRING_CLOUD_NACOS_CONFIG_USERNAME));
+            map.put(SPRING_CLOUD_NACOS_CONFIG_PASSWORD, prop.getProperty(property + "." + SPRING_CLOUD_NACOS_CONFIG_PASSWORD));
             map.values().removeIf(Objects::isNull);
-            inputStream.close();
             return map;
-        } catch (IOException fe) {
-            throw new RuntimeException("读取配置nacos.properties异常");
+        } catch (IOException e) {
+            throw new IllegalStateException("读取配置文件 " + NACOS_PROPERTIES + " 异常", e);
         }
     }
 }
