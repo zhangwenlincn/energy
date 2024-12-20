cfaa6faa04c02fea4b0821964849d65ec1468a4b
zhangwenlin <zhangwenlin@skiffenergy.com>
2024-12-20 17:00:46 +0800

    commit message

--- energy-spring-boot-starter/src/main/java/com/energy/starter/configuration/EnergyStarterConfiguration.java
+++ energy-spring-boot-starter/src/main/java/com/energy/starter/configuration/EnergyStarterConfiguration.java
@@ -1,7 +1,10 @@
 package com.energy.starter.configuration;
 
+import com.energy.base.jackson.JacksonEnum;
 import com.energy.starter.exception.GlobalExceptionHandler;
 import com.energy.starter.spring.SpringUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
@@ -12,4 +15,9 @@ import org.springframework.context.annotation.Import;
 @Configuration
 @Import({GlobalExceptionHandler.class, SpringUtil.class})
 public class EnergyStarterConfiguration {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return JacksonEnum.INSTANCE.getObjectMapper();
+    }
 }
